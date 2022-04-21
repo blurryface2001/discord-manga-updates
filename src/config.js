8d63ae889dbe5e37fb8e3f993d650e8b0466f423
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2022-04-21 15:31:00 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,22 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-const { CLIENT_ID, GUILD_ID, DISCORD_TOKEN, AIRTABLE_KEY } = process.env;
+const {
+  CLIENT_ID,
+  GUILD_ID,
+  DISCORD_TOKEN,
+  AIRTABLE_KEY,
+  REDDIT_USERNAME,
+  REDDIT_PASSWORD,
+} = process.env;
 
 const config = {
   CLIENT_ID,
   GUILD_ID,
   DISCORD_TOKEN,
   AIRTABLE_KEY,
+  REDDIT_USERNAME,
+  REDDIT_PASSWORD,
 };
 
 export default config;
