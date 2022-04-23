6c3dd7ba7cecb8f813136c3c39c7efae9208c2a2
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2022-04-23 17:11:40 +0530

    Delete src directory

--- src/commands/index.js
+++
@@ -1,3 +0,0 @@
-export * as ping from "./ping.js";
-export * as list from "./list.js";
-export * as add from "./add.js";
--- src/sendChannelMessage.js
+++
@@ -1,3 +0,0 @@
-export default async function sendChannelMessage(client, channelId, message) {
-  client.channels.cache.get(channelId).send(message);
-}
