64e40a844784ebf55642411e5c7e277d62b15941
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2024-01-27 22:44:20 +0530

    Update fetchLatestAsuraChapter.js

--- src/commands/utils/fetchLatestAsuraChapter.js
+++ src/commands/utils/fetchLatestAsuraChapter.js
@@ -1,20 +1,24 @@
-import axios from "axios";
-import { load } from "cheerio";
+import { load } from 'cheerio';
+import puppeteer from 'puppeteer-extra';
 
-const headers = {
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-};
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+puppeteer.use(StealthPlugin());
 
 export default async function fetchLatestAsuraChapter(url) {
-  const data = await axios.get(url, { headers });
-  const $ = load(data.data);
-  const chapterList = $("#chapterlist");
-  const latestChapter = chapterList.children().find("li:first");
-  const latestChapterURL = latestChapter.find("a").attr("href");
-  const latestChapterNum = Number(
-    latestChapter.find(".chapternum").text().trim().split(" ")[1]
-  );
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.waitForTimeout(2500);
+  const html = await page.content();
+  await page.close();
+  await browser.close();
 
+  const $ = load(html);
+  const chapterList = $('#chapterlist');
+  const latestChapter = chapterList.children().find('li:first');
+  const latestChapterURL = latestChapter.find('a').attr('href');
+  const latestChapterNum = Number(
+    latestChapter.find('.chapternum').text().trim().split(' ')[1]
+  );
   return { latestChapterURL, latestChapterNum };
 }
