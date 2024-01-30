3905cd7951fafc86866d26ed56cfdd228ac0ce0d
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2024-01-30 22:50:20 +0530

    Update fetchLatestAsuraChapter.js

--- src/commands/utils/fetchLatestAsuraChapter.js
+++ src/commands/utils/fetchLatestAsuraChapter.js
@@ -1,30 +1,20 @@
+import axios from 'axios';
 import { load } from 'cheerio';
-import puppeteer from 'puppeteer-extra';
 
-import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-puppeteer.use(StealthPlugin());
+const headers = {
+  'User-Agent':
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+};
 
 export default async function fetchLatestAsuraChapter(url) {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setJavaScriptEnabled(false);
-  const manhwaPage = await page.goto(url);
-  try {
-    await page.waitForXPath('//*[@id="chapterlist"]', { timeout: 10000 });
-  } catch (e) {
-    // send the error along with page status
-    throw new Error(`Error: ${e} \n\nPage status: ${manhwaPage.status()}`);
-  }
-  const html = await page.content();
-  await page.close();
-  await browser.close();
-
-  const $ = load(html);
+  const data = await axios.get(url, { headers });
+  const $ = load(data.data);
   const chapterList = $('#chapterlist');
   const latestChapter = chapterList.children().find('li:first');
   const latestChapterURL = latestChapter.find('a').attr('href');
   const latestChapterNum = Number(
     latestChapter.find('.chapternum').text().trim().split(' ')[1]
   );
+
   return { latestChapterURL, latestChapterNum };
 }
