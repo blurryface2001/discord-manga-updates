7f4c95c9610b8cc738925521458e72cdd8c794bb
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2024-03-31 10:50:22 +0530

    Update scrapeTotalAnimeEpisode.js

--- src/commands/utils/scrapeTotalAnimeEpisode.js
+++ src/commands/utils/scrapeTotalAnimeEpisode.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { load } from "cheerio";
+import axios from 'axios';
+import { load } from 'cheerio';
 
 export default async function scrapeTotalAnimeEpisode(animeId) {
   return new Promise(async (resolve, reject) => {
@@ -10,27 +10,33 @@ export default async function scrapeTotalAnimeEpisode(animeId) {
 
     try {
       const episodesAjax = await axios.get(
-        `https://aniwatch.to/ajax/v2/episode/list/${animeId.split("-").pop()}`,
+        `https://hianime.to/ajax/v2/episode/list/${animeId.split('-').pop()}`,
         {
           headers: {
-            "User-Agent":
-              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.71 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest",
-            Referer: `https://aniwatch.to/watch/${animeId}`,
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4692.71 Safari/537.36',
+            'X-Requested-With': 'XMLHttpRequest',
+            Referer: `https://hianime.to/watch/${animeId}`,
           },
         }
       );
 
       const $ = load(episodesAjax.data.html);
 
-      res.latestEpisodeNum = Number($(".detail-infor-content .ss-list a").length);
+      res.latestEpisodeNum = Number(
+        $('.detail-infor-content .ss-list a').length
+      );
 
-      $(".detail-infor-content .ss-list a").each((i, el) => {
-        if (Number($(el).attr("data-number")) === res.latestEpisodeNum) {
+      $('.detail-infor-content .ss-list a').each((i, el) => {
+        if (Number($(el).attr('data-number')) === res.latestEpisodeNum) {
           res.episode = {
-            title: $(el)?.attr("title")?.trim() || null,
-            episodeURL: `https://aniwatch.to/watch/${$(el)?.attr("href")?.split("/")?.pop()}` || null,
-            number: Number($(el).attr("data-number")),
+            title: $(el)?.attr('title')?.trim() || null,
+            episodeURL:
+              `https://hianime.to/watch/${$(el)
+                ?.attr('href')
+                ?.split('/')
+                ?.pop()}` || null,
+            number: Number($(el).attr('data-number')),
           };
         }
       });
