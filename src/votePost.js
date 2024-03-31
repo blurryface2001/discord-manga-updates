08104d84f828a47cc1c73a5a3749bc053255f087
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2024-03-31 10:55:42 +0530

    Update votePost.js

--- src/votePost.js
+++ src/votePost.js
@@ -1,11 +1,14 @@
-import config from "./config.js";
-import puppeteer from "puppeteer";
-import sendChannelMessage from "./sendChannelMessage.js";
+import config from './config.js';
+import sendChannelMessage from './sendChannelMessage.js';
+import puppeteer from 'puppeteer-extra';
+
+import StealthPlugin from 'puppeteer-extra-plugin-stealth';
+puppeteer.use(StealthPlugin());
 
 export default async function votePost(reaction, client) {
-  if (reaction.message.channelId !== "965269327580381304") return;
+  if (reaction.message.channelId !== '965269327580381304') return;
   const { name } = reaction.emoji;
-  if (name !== "👍" && name !== "👎") return;
+  if (name !== '👍' && name !== '👎') return;
 
   const { content } = reaction.message;
 
@@ -13,15 +16,17 @@ export default async function votePost(reaction, client) {
     .match(/(?<=New (chapter|art|news|post) of)(.*)(?=is available!)/)[0]
     .trim();
   const url = content.match(/(?<=Discussion: <)(.*)(?=>)/)[0].trim();
-  const id = url.split("https://reddit.com/r/manga/comments/")[1].split("/")[0];
+  const id = url.split('https://reddit.com/r/manga/comments/')[1].split('/')[0];
 
-  console.log("🔃 Voting for: ", title);
-  sendChannelMessage(client, "966631308245741598", `🔃 Voting for: ${title}`);
+  console.log('🔃 Voting for: ', title);
+  sendChannelMessage(client, '966631308245741598', `🔃 Voting for: ${title}`);
 
+  let browser;
+  let page;
   try {
     // Launch the browser
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
     await page.setUserAgent(
       'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +
         '(KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
@@ -31,41 +36,48 @@ export default async function votePost(reaction, client) {
     });
 
     // Login to reddit
-    await page.goto("https://old.reddit.com/login");
-    await page.type("#user_login", config.REDDIT_USERNAME);
-    await page.type("#passwd_login", config.REDDIT_PASSWORD);
-    await page.click("form#login-form button[type=submit]");
+    await page.goto('https://old.reddit.com/login');
+    await page.type('#user_login', config.REDDIT_USERNAME);
+    await page.type('#passwd_login', config.REDDIT_PASSWORD);
+    await page.click('form#login-form button[type=submit]');
     await page.waitForNavigation();
 
     // Go to the subreddit
     await page.goto(`https://old.reddit.com/${id}/`);
-    if (name === "👍") {
-      await page.click("div[data-event-action=upvote]");
+    if (name === '👍') {
+      await page.click('div[data-event-action=upvote]');
       console.log(`👍 ${title}`);
       sendChannelMessage(
         client,
-        "966631308245741598",
+        '966631308245741598',
         `⬆️ Upvoted the manga: ${title}! \n\n${url}`
       );
     } else {
-      await page.click("div[data-event-action=downvote]");
+      await page.click('div[data-event-action=downvote]');
       console.log(`👎 ${title}`);
       sendChannelMessage(
         client,
-        "966631308245741598",
+        '966631308245741598',
         `⬇️ Downvoted the manga: ${title}! \n\n${url}`
       );
     }
 
+    await page.close();
     await browser.close();
   } catch (error) {
+    if (page) {
+      await page.close();
+    }
+    if (browser) {
+      await browser.close();
+    }
     console.error(error);
 
     // Send error to #error-logs channel
     sendChannelMessage(
       client,
-      "966622664800215040",
-      "💥 Error while voting! \n\n" + error
+      '966622664800215040',
+      '💥 Error while voting! \n\n' + error
     );
   }
 }
