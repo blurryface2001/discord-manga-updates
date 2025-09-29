69bebf65248b94de7b0c32c59e074d3288edacaa
blurryface2001 <103877697+blurryface2001@users.noreply.github.com>
2025-09-29 18:03:26 +0530

    Update fetchControls.js
    Migration to baserow

--- src/commands/lib/fetchControls.js
+++ src/commands/lib/fetchControls.js
@@ -1,39 +1,71 @@
-import config from '../../config.js';
-import Airtable from 'airtable';
+// import config from '../../config.js';
+// import Airtable from 'airtable';
 
-const base = new Airtable({ apiKey: config.AIRTABLE_KEY }).base(
-  'appwmWHs7HvwwZAnH'
-);
+// const base = new Airtable({ apiKey: config.AIRTABLE_KEY }).base(
+//   'appwmWHs7HvwwZAnH'
+// );
+
+// export default async function fetchControls() {
+//   return new Promise((resolve, reject) => {
+//     const media = {};
+
+//     base('collection')
+//       .select({
+//         view: 'Grid view',
+//         fields: ['media', 'shouldFetch'],
+//       })
+//       .eachPage(
+//         function page(records, fetchNextPage) {
+//           // This function (`page`) will get called for each page of records.
+
+//           records.forEach(function (record) {
+//             media[record.get('media')] = Boolean(record.get('shouldFetch'));
+//           });
+
+//           // To fetch the next page of records, call `fetchNextPage`.
+//           // If there are more records, `page` will get called again.
+//           // If there are no more records, `done` will get called.
+//           fetchNextPage();
+//         },
+//         function done(err) {
+//           if (err) {
+//             reject(err);
+//           } else {
+//             resolve(media);
+//           }
+//         }
+//       );
+//   });
+// }
+
+import config from '../../config.js';
+import fetch from 'node-fetch';
+
+const BASEROW_API_TOKEN = config.BASEROW_KEY;
 
 export default async function fetchControls() {
-  return new Promise((resolve, reject) => {
-    const media = {};
+  const media = {};
+  let url = `https://api.baserow.io/api/database/rows/table/688610/?user_field_names=true`;
 
-    base('collection')
-      .select({
-        view: 'Grid view',
-        fields: ['media', 'shouldFetch'],
-      })
-      .eachPage(
-        function page(records, fetchNextPage) {
-          // This function (`page`) will get called for each page of records.
+  while (url) {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Token ${BASEROW_API_TOKEN}`,
+      },
+    });
 
-          records.forEach(function (record) {
-            media[record.get('media')] = Boolean(record.get('shouldFetch'));
-          });
+    if (!res.ok) {
+      throw new Error(`Baserow API error: ${res.status} ${await res.text()}`);
+    }
 
-          // To fetch the next page of records, call `fetchNextPage`.
-          // If there are more records, `page` will get called again.
-          // If there are no more records, `done` will get called.
-          fetchNextPage();
-        },
-        function done(err) {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(media);
-          }
-        }
-      );
-  });
+    const data = await res.json();
+
+    data.results.forEach((row) => {
+      media[row.media] = Boolean(row.shouldFetch);
+    });
+
+    url = data.next; // pagination
+  }
+
+  return media;
 }
